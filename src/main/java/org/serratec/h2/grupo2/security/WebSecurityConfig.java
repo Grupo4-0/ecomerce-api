4a49b23006691ca2935a58243cb7469ad690d063
Nathan Gomes <nathansantosgomez@gmail.com>
2025-06-16 19:17:23 -0300

    salvando mudanças antes de mesclar com a main

--- src/main/java/org/serratec/h2/grupo2/security/WebSecurityConfig.java
+++ src/main/java/org/serratec/h2/grupo2/security/WebSecurityConfig.java
@@ -19,6 +19,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.micrometer.common.lang.NonNull;
+
 @Configuration
 @EnableMethodSecurity
 public class WebSecurityConfig {
@@ -112,7 +114,7 @@ public class WebSecurityConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**") 
                         .allowedOrigins("http://localhost:5173") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
